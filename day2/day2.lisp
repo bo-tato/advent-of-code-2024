994c0c886096c11bf2ffa04d6a6d04f735362615
bo-tato <122528427+bo-tato@users.noreply.github.com>
2024-12-02 17:05:02 -0500

    follow conventions for predicate function naming

--- day2/day2.lisp
+++ day2/day2.lisp
@@ -6,9 +6,9 @@
       (loop for (a b) on report while b
             always (<= 1 (- b a) 3))))
 
-(defun part2-safep (report)
+(defun part2-safe-p (report)
   (loop for level below (length report)
         thereis (safep (remove-nth level report))))
 
 (loop for report in (read-file-lines "input.txt")
-      count (part2-safep (string-to-num-list report)))
+      count (part2-safe-p (string-to-num-list report)))
